445c40e07d267c9b8c5dd9c2632cb4bc79711dd3
Victor Ozoterio <victorozoterio@gmail.com>
2025-01-09 10:19:19 -0300

    feat: exibindo o footer

--- scripts.js
+++ scripts.js
@@ -2,10 +2,12 @@
 const USD = 4.87;
 const EUR = 5.32;
 const GBP = 6.08;
+
 // Obtendo os elementos do formulário.
 const form = document.querySelector("form");
 const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
+const footer = document.querySelector("main footer");
 
 // Manipulando o input amount para receber somente números.
 amount.addEventListener("input", () => {
@@ -29,7 +31,17 @@ form.onsubmit = (event) => {
 			break;
 	}
 };
+
 // Função para converter a moeda.
 function convertCurrency(amount, price, currency) {
-	console.log(amount, price, currency);
+	try {
+		// Aplica a classe que exibe o footer para mostrar o resultado.
+		footer.classList.add("show-result");
+	} catch (error) {
+		// Remove a classe do footer removendo ele da tela.
+		footer.classList.remove("show-result");
+
+		console.log(error);
+		alert("Não foi possível converter. Tente novamente mais tarde.");
+	}
 }
