b453626614367654809caeccc764eb6a71bfcb23
Victor Ozoterio <victorozoterio@gmail.com>
2025-01-09 10:17:12 -0300

    feat: obtendo a moeda selecionada

--- scripts.js
+++ scripts.js
@@ -1,6 +1,14 @@
+const form = document.querySelector("form");
 const amount = document.getElementById("amount");
+const currency = document.getElementById("currency");
+
 // Manipulando o input amount para receber somente números.
 amount.addEventListener("input", () => {
 	const hasCharactersRegex = /\D+/g;
 	amount.value = amount.value.replace(hasCharactersRegex, "");
 });
+// Capturando o evento de submit (enviar) do formulário.
+form.onsubmit = (event) => {
+	event.preventDefault();
+	console.log(currency.value);
+};
