cfd645b4d53c9e6d7fa618aca88de692891c5c63
Victor Ozoterio <victorozoterio@gmail.com>
2025-01-09 10:18:40 -0300

    feat: identificando a moeda

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+// Cotação de moedas do dia.
+const USD = 4.87;
+const EUR = 5.32;
+const GBP = 6.08;
+// Obtendo os elementos do formulário.
 const form = document.querySelector("form");
 const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
@@ -7,8 +12,24 @@ amount.addEventListener("input", () => {
 	const hasCharactersRegex = /\D+/g;
 	amount.value = amount.value.replace(hasCharactersRegex, "");
 });
+
 // Capturando o evento de submit (enviar) do formulário.
 form.onsubmit = (event) => {
 	event.preventDefault();
-	console.log(currency.value);
+
+	switch (currency.value) {
+		case "USD":
+			convertCurrency(amount.value, USD, "US$");
+			break;
+		case "EUR":
+			convertCurrency(amount.value, EUR, "€");
+			break;
+		case "GBP":
+			convertCurrency(amount.value, USD, "£");
+			break;
+	}
 };
+// Função para converter a moeda.
+function convertCurrency(amount, price, currency) {
+	console.log(amount, price, currency);
+}
