6dcdc08cad9e5356160be25d1394e376f1034e07
Victor Ozoterio <victorozoterio@gmail.com>
2025-01-09 10:16:48 -0300

    feat: validando o input de valor

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const amount = document.getElementById("amount");
+// Manipulando o input amount para receber somente números.
+amount.addEventListener("input", () => {
+	const hasCharactersRegex = /\D+/g;
+	amount.value = amount.value.replace(hasCharactersRegex, "");
+});
